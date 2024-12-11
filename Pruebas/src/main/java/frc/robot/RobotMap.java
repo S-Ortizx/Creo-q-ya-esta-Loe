82672078aa3697b4dafb820c7ab26f2b67caed92
Santiago <santiagoortizmoctezuma@hotmail.com>
2024-12-11 11:11:26 -0600

    done 2

---
+++ Pruebas/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class RobotMap {
+    public static final int XboxPort = 0;
+    public static int motor_right_1 = 16;
+    public static int motor_right_2 = 2;
+    public static int motor_left_1 = 6;
+    public static int motor_left_2 = 7;
+
+}
