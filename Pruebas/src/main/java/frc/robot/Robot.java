91ccd9245457b7bbc704c73dcedaaf869c5e9941
Santiago <santiagoortizmoctezuma@hotmail.com>
2024-12-11 11:25:12 -0600

    NO

--- Pruebas/src/main/java/frc/robot/Robot.java
+++ Pruebas/src/main/java/frc/robot/Robot.java
@@ -29,7 +29,7 @@ public class Robot extends TimedRobot {
 
   // another change! , please add resources!!
   Navx navx ;
-//Pruba loe// me llamo joe 
+//Pruba loe// me llamo joe // No 
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
