5dc8a831b4f1dc3744621eda30ae3b7654d9114a
Santiago <santiagoortizmoctezuma@hotmail.com>
2025-02-06 17:14:40 -0600

    2025_TryRobot

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+public class RobotMap {
+    public static final int XboxPort = 0;
+    public static final int XboxPort2 = 1;
+    ////
+    public static int motor_right_1 = 16;
+    public static int motor_left_1 = 6;
+    ////
+    public static int elevatorMotor1 = 17;
+    public static int elevatorMotor2 = 13;
+    public static int coralIntake1 = 3;
+    public static int coralIntake2 = 10;
+    public static double coralspeed = 0.4;
+    ////
+    public static int moduleintake = 7;
+    public static int forward = 3;
+    public static int reverse = 12;
+    public static int algieintake = 13;
+    public static double algiespeed = 0.7;
+    ////
+    public static double level1 = 12;
+    public static double level2 = 56;
+    public static double level3 = 567;
+}
